451794514491eeb32b4a7655b0fe1ade5614d20b
patrickleon710 <gachohinderitu005@gmail.com>
2023-05-30 23:04:07 +0300

    finished styles

--- build/main.js
+++ build/main.js
@@ -1,11 +1,27 @@
-const initApp = ()=> {
+/* function initApp() {
     const hamburgerBtn = document.getElementById('hamburger-button')
     const mobileMenu = document.getElementById('mobile-menu')
 
-    const toggleMenu = ()=> {
+    const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden')
         mobileMenu.classList.toggle('flex')
     }
+    hamburgerBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener('click', toggleMenu)
+}
+
+document.addEventListener('DOMContentLoaded', initApp) */
+
+
+const initApp = () => {
+    const hamburgerBtn = document.getElementById('hamburger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+    }
+
     hamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
 }
